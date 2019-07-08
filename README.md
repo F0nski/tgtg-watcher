cdd4fc4236ab1229420cf81390f6c07026c11bfb
Lucas Gratien <lucas.g@capkopper.fr>
2019-07-08 17:02:21 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,42 @@
+# TooGoodToGo Watcher
+
+## Introduction
+
+**TooGoodToGo Watcher** is a simple NodeJS Tool to watch for your favourites in TooGoodToGo.
+
+It uses the TGTG API to check your favourites, and send you an email (atm only through gmail) as soon as there is available stocks.
+
+## Installation
+
+Simply install NodeJS (see https://nodejs.org/en/download/) and run in your terminal :
+
+```npm install```
+
+Or if you have Yarn installed
+
+```yarn install```
+
+## Configuration
+
+First of all, copy the `example_config.json` file to `config.json` and edit the configuration.
+
+* `TGTG_USER`: Your TGTG username (or email)
+* `TGTG_PASSWORD`: Your TGTG password
+* `FREQUENCY`: The frequency at which you want to watcher to check your favourites (in minutes). Don't go below 0.25 to avoid DdoS...
+* `EMAIL_SENDER`: The GMail account used to send mails
+* `EMAIL_PASSWORD`: The GMail account application password (see https://support.google.com/mail/answer/185833)
+* `EMAIL_RECIPIENTS`: The emails recipients (seperated by commas). You can just use the same as the email sender.
+* `ANDROID_APP_VERSION`: The current Android APP version. If the app is updated, the TGTG watcher may not work, so you have to update this value.
+* `MIN_TIME_BETWEEN_MAIL`: The minimum time between 2 emails for the same business (in minutes). For example, if you put 360, it will wait 6 hours before sending you an email notification (so you don't get spam by yourself...)
+
+## Use
+
+To ensure that everything is well configured, just go in the project's folder and type:
+
+```npm tgtg-watcher.js show```
+
+If it shows your list of favourite, you're all good. Else, just check your configuration or the Android APP version number.
+
+Then, and finally, to run the watcher, type:
+
+```npm tgtg-watcher.js email```
---
+++ tgtg-watcher.js
@@ -0,0 +1,164 @@
+const fetch = require('node-fetch');
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+const { promisify } = require('util')
+
+var email = config.TGTG_USER;
+var password = config.TGTG_PASSWORD;
+
+const base_url = "https://apptoogoodtogo.com/index.php/api_tgtg/";
+
+const readFileAsync = promisify(fs.readFile)
+const writeFileAsync = promisify(fs.writeFile)
+
+const main = async () => {
+  console.log('\n\n\n--------- TGTG WATCHER --------\n\n\n')
+
+  const res = await readFileAsync('config.json');
+  const config = JSON.parse(res);
+
+  var base_headers = {
+    'accept': 'application/json',
+    'content-type': 'application/x-www-form-urlencoded; charset=utf-8',
+    'accept-language': 'fr-FR;q=1.0, en-FR;q=0.9',
+    'user-agent': 'TGTG/' + config.ANDROID_APP_VERSION + ' (637) (Android/Unknown; Scale/3.00)',
+  }
+
+  // First, connect to TGTG
+  console.log('-- Connect to ToGoodToGo --\n');
+
+  var login = await fetch(base_url + 'login', {
+    method: 'POST',
+    headers: base_headers,
+    body: "email=" + config.TGTG_USER + "&password=" + config.TGTG_PASSWORD
+  });
+
+  var json = await login.json();
+  var user_id = json.user_id;
+  var user_token = json.user_token;
+
+  if (user_id == undefined) {
+    console.log('Wrong credentials');
+    return;
+  }
+
+  console.log("User id:" + user_id);
+  console.log("User token:" + user_token + "\n\n");
+
+  base_headers['authorization'] = Buffer.from(`ACCESS:${user_id}:${user_token}`).toString('base64');
+
+  switch (process.argv[2]) {
+    case "email":
+      var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: config.EMAIL_SENDER,
+          pass: config.EMAIL_PASSWORD
+        }
+      });
+
+      fs.exists('data.json', function(exists){
+        if(!exists){
+          fs.writeFile('data.json', '{}');
+        }
+      });
+
+      var minutes = config.FREQUENCY, the_interval = minutes * 60 * 1000;
+
+      if (minutes < 0.20) {
+        console.log("Frequency too low. We don't want to DdoS here...");
+        return;
+      }
+
+      setInterval(async () => {
+        var dateTime = new Date().toLocaleString();
+        console.log('[' + dateTime + '] Run');
+
+        // Read the data file.
+        fs.readFile('data.json', function readFileCallback(err, data) {
+          if (err) {
+            console.log(err);
+            return;
+          } else {
+            timetable = JSON.parse(data);
+          }
+        });
+
+        var response = await fetch(base_url + 'list_favorite_businessesv5', {
+          method: 'POST',
+          headers: base_headers,
+          body: `user_id=${user_id}&user_token=${user_token}`,
+        });
+
+        var json = await response.json();
+
+        for (const business of json.info) {
+          send = false;
+          if (business['todays_stock'] > 0) {
+            if (!timetable.hasOwnProperty(business['id'])) {
+              send = true;
+            }
+            else {
+              // 6 hours later.
+              if (Date.now() - parseInt(timetable[business['id']]) > (config.MIN_TIME_BETWEEN_MAIL * 60 * 1000)) {
+                send = true;
+              }
+            }
+            if (send) {
+              console.log(business['business_name'], '\n');
+
+              // Prepare the email.
+              var mailOptions = {
+                from: config.EMAIL_SENDER,
+                to: config.EMAIL_RECIPIENTS,
+                subject: '[TooGoodToGo] ' + business['business_name'],
+                text: JSON.stringify(business, undefined, 2)
+              };
+
+              // Update the timetable.
+              timetable[business['id']] = Date.now();
+
+              // Send the email
+              transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                  console.log(error);
+                } else {
+                  console.log('Email sent: ' + info.response);
+                }
+              });
+            }
+          }
+        }
+
+        fs.writeFile("data.json", JSON.stringify(timetable), (error) => { /* handle error */ });
+      }, the_interval);
+
+      break;
+
+    case "show":
+    default:    
+      // Simple display.
+
+      // Get favourites from TGTG
+      console.log('-- Retrieve favourites --\n');
+      var response = await fetch(base_url + 'list_favorite_businessesv5', {
+        method: 'POST',
+        headers: base_headers,
+        body: `user_id=${user_id}&user_token=${user_token}`,
+      });
+
+      var json = await response.json();
+
+      var i = 1;
+      for(const business of json.info) {
+        var color = business['todays_stock'] > 0 ? "\x1b[32m" : "\x1b[31m";
+
+        console.log(color, i + ": ", business['business_name']);
+        i++;
+      }
+      break;
+
+  }
+}
+
+main();
